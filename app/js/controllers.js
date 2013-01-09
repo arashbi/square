ea4b7b1d1b21a74cf70a7cc9d2fee43766dacb35
Arash <arashbi@gmail.com>
2013-01-09 16:02:59 -0500

    posting username password

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,12 +2,22 @@
 
 /* Controllers */
 
-function LoginController($scope) {
+function LoginController($scope,$http) {
 	$scope.login = function(){
-		alert("hello " + $scope.username + "/" + $scope.password);
-	}
+		alert("hello " + $scope.data.username + "/" + $scope.data.password);
+		$http.post("http://localhost:8000/blah",$scope.data)
+		.success(
+			function (data,status,headers,config){})
+		.error(
+			function (data,status,headers,config){
+				console.log(data);
+				console.log(status);
+				console.log(headers);
+				console.log(config);
+			});
+	};
 }
-LoginController.$inject = ['$scope'];
+LoginController.$inject = ['$scope','$http'];
 
 function MyCtrl1() {}
 MyCtrl1.$inject = [];
